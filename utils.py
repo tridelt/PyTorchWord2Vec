45c5e90a782456020ba00b05584d3e9acba98a4a
Luca Rauchenberger <luca@Lucas-iMac.home>
2019-02-27 02:20:27 +0100

    feat: calculate and visualize training and validation loss

--- utils.py
+++ utils.py
@@ -1,3 +1,28 @@
+import numpy as np
+
+    # creates wordPairs for the Training & Validation Set
+def createWordPairs(indexes, tokens, window_size):
+    idx_pairs = []
+        
+    # for each sentence        
+    for sentence in tokens.tokenzied_corpus:
+        try:
+            indices = [indexes.word2idx[word] for word in sentence]
+        except Exception as e: print(e)
+        # for each word, threated as center word
+        for center_word_pos in range(len(indices)):
+            # for each window position
+            for w in range(-window_size, window_size + 1):
+                context_word_pos = center_word_pos + w
+                # make soure not jump out sentence
+                if context_word_pos < 0 or context_word_pos >= len(indices) or center_word_pos == context_word_pos:
+                    continue
+                context_word_idx = indices[context_word_pos]
+                idx_pairs.append((indices[center_word_pos], context_word_idx))
+    return np.array(idx_pairs)
+
+    
+
 
 class AverageMeter():
     """Computes and stores the average and current value"""
@@ -9,6 +34,8 @@ class AverageMeter():
         self.avg = 0
         self.sum = 0
         self.count = 0
+        self.train_loss = 0
+        self.valid_loss = 0
 
     def update(self, val, n=1):
         self.val = val
